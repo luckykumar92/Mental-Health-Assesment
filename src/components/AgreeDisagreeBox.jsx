fc6c5ea300d5e201310f336fb4597f6f5b882cd2
Lucky Kumar <lucky.kumar@lpu.in>
2024-02-02 16:29:02 +0530

    commit

--- src/components/AgreeDisagreeBox.jsx
+++ src/components/AgreeDisagreeBox.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import { useForm } from "react-hook-form";
+import homeImage from "./assets/home.png";
 
 const AgreeDisagreeBox = () => {
   const { register, handleSubmit } = useForm();
@@ -127,111 +128,151 @@ const AgreeDisagreeBox = () => {
   };
 
   return (
-    <div>
-      <span className=" mx-auto">
-        <p className="text-center font-bold text-2xl mb-8">
-          Using the key below, answer the questions based on how strongly
-          <br />
-          you agree or disagree with the statement.
-        </p>
-        <ul className="">
-          <div className="bg-[#8adf56] flex flex-row place-content-center justify-center text-white items-center space-x-20 w-fit p-3 mx-auto rounded-sm mb-3  border border-blue-600">
-            <li>Strongly Agree </li>
-            <li>Neutral </li>
-            <li>Strongly Agree</li>
+    <div className="px-20 pt-20 max-[480px]:px-1 max-[480px]:pt-2 bg-gray-300 mx-auto">
+      <div className="mx-auto bg-white">
+        <div style={{ position: "relative", width: "100%", height: "100%" }}>
+          <img src={homeImage} alt="" srcset="" className="w-full h-full" />
+          <div
+            style={{
+              position: "absolute",
+              top: "50%",
+              // left: "50%",
+              // transform: "translate(-50%, -50%)",
+              left: 0,
+              right: 0,
+              margin: "auto",
+              textAlign: "center",
+            }}>
+            <p className="text-8xl max-[700px]:text-2xl  text-white mx-auto mb-8">
+              Your Mental Health Today Test
+            </p>
+            <span className="mx-auto mt-20">
+              <button className="bg-[#009a80] px-4 py-2 rounded-md text-white">
+                20 Questions
+              </button>
+              <button className="bg-[#1b6099] px-4 py-2 rounded-md text-white ml-4">
+                3 Minutes
+              </button>
+            </span>
           </div>
-        </ul>
-      </span>
-      <div>
-        <form onSubmit={handleSubmit(questionFormResponseData)}>
-          {data.map((q, index) => (
-            <div key={q.name} className="">
-              <h1 className="text-center">{q.question}</h1>
-              <div className="flex flex-col justify-center items-center py-4 max-[700px]:p-2 w-fit mx-auto">
-                <div className="flex flex-row space-x-4 max-[700px]:space-x-2 justify-center items-center">
-                  <div className="flex flex-col text-center">
-                    <input
-                      disabled={q.disabled}
-                      onClick={() => radioClickedHandler(index)}
-                      type="radio"
-                      value="1"
-                      className="!bg-green-500 !border-2 !border-red-700 w-28 h-28 max-[700px]:w-24 max-[700px]:h-24 max-[480px]:w-16 max-[480px]:h-16 cursor-pointer"
-                      {...register(`${q.name}`, {
-                        required: true,
-                      })}
-                    />
-                    <span className="">Agree</span>
-                  </div>
-                  <div>
-                    <input
-                      disabled={q.disabled}
-                      onClick={() => radioClickedHandler(index)}
-                      type="radio"
-                      value="2"
-                      className="w-24 h-24 max-[700px]:w-20 max-[700px]:h-20 max-[480px]:w-14 max-[480px]:h-14 cursor-pointer"
-                      {...register(`${q.name}`, {
-                        required: true,
-                      })}
-                    />
-                  </div>
-                  <div>
-                    <input
-                      disabled={q.disabled}
-                      onClick={() => radioClickedHandler(index)}
-                      type="radio"
-                      value="3"
-                      className="w-20 h-20 max-[700px]:w-16 max-[700px]:h-16 max-[480px]:w-12 max-[480px]:h-12 cursor-pointer"
-                      {...register(`${q.name}`, {
-                        required: true,
-                      })}
-                    />
-                  </div>
-                  <div>
-                    <input
-                      disabled={q.disabled}
-                      onClick={() => radioClickedHandler(index)}
-                      type="radio"
-                      value="4"
-                      className="w-24 h-24 max-[700px]:w-20 max-[700px]:h-20 max-[480px]:w-14 max-[480px]:h-14 cursor-pointer"
-                      {...register(`${q.name}`, {
-                        required: true,
-                      })}
-                    />
-                  </div>
-                  <div className="flex flex-col text-center">
-                    <input
-                      disabled={q.disabled}
-                      onClick={() => radioClickedHandler(index)}
-                      type="radio"
-                      value="5"
-                      className="w-28 h-28 max-[700px]:w-24 max-[700px]:h-24 max-[480px]:w-16 max-[480px]:h-16 cursor-pointer"
-                      {...register(`${q.name}`, {
-                        required: true,
-                      })}
-                    />
-                    <span className="">Disagree</span>
-                  </div>
-                </div>
-                <hr class="h-px my-8 border-0 bg-[#0b7361]  mx-auto w-[90%]" />
-              </div>
+        </div>
+        <div className="bg-[#009A80] rounded-b-[90%] max-[700px]:rounded-b-[50%] rounded-tl-lg rounded-tr-lg mb-8 ">
+          <p className=" text-white text-center text-xl max-[700px]:text-base pt-8 px-4">
+            How Well Do You Cope ?
+          </p>
+          <p className=" text-white text-center text-xl max-[700px]:text-base mt-8 px-16 pb-40 max-[700px]:px-2 max-[700px]:pb-32 ">
+            Most people struggle with mental health challenges at some point in
+            life, whether due to circumstances or underlying vulnerabilities and
+            psychiatric conditions. This test will give you a sense of how
+            you're coping day to day and whether you might be experiencing
+            symptoms that could be addressed or alleviated via therapy or other
+            professional help.
+          </p>
+        </div>
+        <span className="mx-auto">
+          <p className="text-center font-bold text-xl px-16 max-[700px]:px-2">
+            Using the key below, answer the questions based on how strongly you
+            agree or disagree with the statement.
+          </p>
+          <ul className="">
+            <div className="bg-[#8adf56] flex flex-row place-content-center justify-center text-white items-center space-x-20 w-fit p-3 mx-auto rounded-sm mb-3  border border-blue-600">
+              <li>Strongly Agree </li>
+              <li>Neutral </li>
+              <li>Strongly Agree</li>
             </div>
-          ))}
-          <div className="flex flex-col place-content-center justify-center items-center mb-4">
-            <p className="mb-4">Enter your Email Address </p>
-            <input
-              type="text"
-              id="simple-search"
-              className="p-2 mb-3 border border-gray-300 rounded-md"
-              // required
-            />
-            <button
-              disabled={canNotSubmit}
-              type="submit"
-              class="text-white bg-[#0b7361] px-6 py-2 font-medium rounded-md">
-              Get Your Score
-            </button>
-          </div>
-        </form>
+          </ul>
+        </span>
+        <div>
+          <form onSubmit={handleSubmit(questionFormResponseData)}>
+            {data.map((q, index) => (
+              <div key={q.name} className="">
+                <h1 className="text-center">{q.question}</h1>
+                <div className="flex flex-col justify-center items-center py-4 max-[700px]:p-2 w-fit mx-auto">
+                  <div className="flex flex-row space-x-4 max-[700px]:space-x-2 justify-center items-center">
+                    <div className="flex flex-col text-center">
+                      <input
+                        disabled={q.disabled}
+                        onClick={() => radioClickedHandler(index)}
+                        type="radio"
+                        value="1"
+                        className="!bg-green-500 !border-2 !border-red-700 w-28 h-28 max-[700px]:w-24 max-[700px]:h-24 max-[480px]:w-16 max-[480px]:h-16 cursor-pointer"
+                        {...register(`${q.name}`, {
+                          required: true,
+                        })}
+                      />
+                      <span className="">Agree</span>
+                    </div>
+                    <div>
+                      <input
+                        disabled={q.disabled}
+                        onClick={() => radioClickedHandler(index)}
+                        type="radio"
+                        value="2"
+                        className="w-24 h-24 max-[700px]:w-20 max-[700px]:h-20 max-[480px]:w-14 max-[480px]:h-14 cursor-pointer"
+                        {...register(`${q.name}`, {
+                          required: true,
+                        })}
+                      />
+                    </div>
+                    <div>
+                      <input
+                        disabled={q.disabled}
+                        onClick={() => radioClickedHandler(index)}
+                        type="radio"
+                        value="3"
+                        className="w-20 h-20 max-[700px]:w-16 max-[700px]:h-16 max-[480px]:w-12 max-[480px]:h-12 cursor-pointer"
+                        {...register(`${q.name}`, {
+                          required: true,
+                        })}
+                      />
+                    </div>
+                    <div>
+                      <input
+                        disabled={q.disabled}
+                        onClick={() => radioClickedHandler(index)}
+                        type="radio"
+                        value="4"
+                        className="w-24 h-24 max-[700px]:w-20 max-[700px]:h-20 max-[480px]:w-14 max-[480px]:h-14 cursor-pointer"
+                        {...register(`${q.name}`, {
+                          required: true,
+                        })}
+                      />
+                    </div>
+                    <div className="flex flex-col text-center">
+                      <input
+                        disabled={q.disabled}
+                        onClick={() => radioClickedHandler(index)}
+                        type="radio"
+                        value="5"
+                        className="w-28 h-28 max-[700px]:w-24 max-[700px]:h-24 max-[480px]:w-16 max-[480px]:h-16 cursor-pointer"
+                        {...register(`${q.name}`, {
+                          required: true,
+                        })}
+                      />
+                      <span className="">Disagree</span>
+                    </div>
+                  </div>
+                  <hr class="h-px my-8 border-0 bg-[#0b7361]  mx-auto w-[90%]" />
+                </div>
+              </div>
+            ))}
+            <div className="flex flex-col place-content-center justify-center items-center mb-4">
+              <p className="mb-4">Enter your Email Address </p>
+              <input
+                type="text"
+                id="simple-search"
+                className="p-2 mb-3 border border-gray-300 rounded-md"
+                // required
+              />
+              <button
+                disabled={canNotSubmit}
+                type="submit"
+                class="text-white bg-[#0b7361] px-6 py-2 font-medium rounded-md">
+                Get Your Score
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
